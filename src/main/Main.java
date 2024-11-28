456b5980a9f668fa55d5e2c619e985e7f9c7ea90
Berk <mberktuncer@gmail.com>
2024-11-28 14:02:31 +0300

    added User class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 
+import main.helper.Config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try(Connection connection = Config.connect()){
+            System.out.println("Database connection success");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
