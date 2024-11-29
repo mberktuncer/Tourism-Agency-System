c622465ebae34727d06fe06dd03f4e4430597a3f
Berk <mberktuncer@gmail.com>
2024-11-29 10:43:04 +0300

    added getUserById service

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,11 +10,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<User> users = UserService.listAll();
 
-        for (User user : users){
-            System.out.println(user.toString());
-        }
+
+        User user = UserService.getUserById(1);
+        System.out.println(user.toString());
 
     }
 }
--- src/main/service/UserService.java
+++ src/main/service/UserService.java
@@ -3,10 +3,7 @@ package main.service;
 import main.helper.Config;
 import main.model.User;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class UserService {
@@ -15,7 +12,7 @@ public class UserService {
         ArrayList<User> users = new ArrayList<>();
         String query = "SELECT * FROM users";
         User user;
-        try(Connection connection = Config.connect()){
+        try(Connection ignored = Config.connect()){
             Statement statement = Config.connect().createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()){
@@ -34,5 +31,33 @@ public class UserService {
         return users;
     }
 
+    public static User getUserById(int id){
+        String query = "SELECT * FROM users WHERE id = ?";
+        User user = null;
 
+        try (Connection ignored = Config.connect()){
+            PreparedStatement preparedStatement = Config.connect().prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setSurname(resultSet.getString("surname"));
+                user.setUserName(resultSet.getString("username"));
+                user.setPassword("password");
+                user.setRole("role");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return user;
+
+    }
+
+    public static boolean add(String name, String surname, String userName, String password, String role){
+        String query = "INSERT INTO users (name, surname, username, password, role) VALUES (?, ?, ?, ?, ?)";
+        return true;
+    }
 }
