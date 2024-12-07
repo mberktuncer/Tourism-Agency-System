778a63a8018efbf64b2e2124f01ddb236760ac57
Berk <mberktuncer@gmail.com>
2024-12-07 21:04:26 +0300

    modified total price label

--- src/main/ui/MakeReservationGUI.java
+++ src/main/ui/MakeReservationGUI.java
@@ -9,11 +9,14 @@ import main.service.ReservationService;
 import main.service.RoomService;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
 public class MakeReservationGUI extends JFrame{
     private JPanel panel1;
@@ -29,6 +32,9 @@ public class MakeReservationGUI extends JFrame{
     private JButton btn_make_reservation;
     private StaffGUI staffGUI;
     private String roomId;
+    private int nights;
+    private double totalPrice;
+    private RoomDetails roomDetails;
 
     public MakeReservationGUI(String roomId, String checkIn, String checkOut){
         this.roomId = roomId;
@@ -36,6 +42,8 @@ public class MakeReservationGUI extends JFrame{
         initializeEvents();
         fld_check_in.setText(checkIn);
         fld_check_out.setText(checkOut);
+        fld_number_of_child.setText("0");
+        fld_number_of_adult.setText("0");
     }
 
     public void initializeGUI(){
@@ -47,7 +55,6 @@ public class MakeReservationGUI extends JFrame{
         setTitle(Constants.WINDOW_TITLE_STAFF);
         setVisible(true);
     }
-
 
     public void initializeEvents(){
         btn_make_reservation.addActionListener(e -> {
@@ -61,10 +68,11 @@ public class MakeReservationGUI extends JFrame{
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
             LocalDate checkInDate = LocalDate.parse(checkInString, formatter);
             LocalDate checkOutDate = LocalDate.parse(checkOutString, formatter);
-            int nights = (int) ChronoUnit.DAYS.between(checkInDate, checkOutDate);
 
-            RoomDetails roomDetails = RoomService.getRoomDetailsById(Integer.parseInt(roomId));
-            double totalPrice = ReservationService.calculatePrice(roomDetails, numberOfAdult, numberOfChildren, nights);
+            nights = (int) ChronoUnit.DAYS.between(checkInDate, checkOutDate);
+            roomDetails = RoomService.getRoomDetailsById(Integer.parseInt(roomId));
+            totalPrice = ReservationService.calculatePrice(roomDetails, numberOfAdult, numberOfChildren, nights);
+
 
             Reservation reservation = new Reservation();
             reservation.setRoomId(Integer.parseInt(roomId));
@@ -84,6 +92,7 @@ public class MakeReservationGUI extends JFrame{
                     if (ReservationService.add(reservation)){
                         GUIHelper.showMessage(Constants.MSG_DONE);
                         ReservationService.decreaseRoomStock(reservation.getRoomId());
+                        dispose();
                     }
                     else{
                         GUIHelper.showMessage(Constants.MSG_ERROR);
@@ -91,6 +100,60 @@ public class MakeReservationGUI extends JFrame{
                 }
             }
         });
+        fld_number_of_adult.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateTotalPrice();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateTotalPrice();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateTotalPrice();
+            }
+        });
+        fld_number_of_child.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateTotalPrice();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateTotalPrice();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateTotalPrice();
+            }
+        });
+    }
+
+    private void updateTotalPrice(){
+        try {
+            String checkInString = fld_check_in.getText();
+            String checkOutString = fld_check_out.getText();
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate checkInDate = LocalDate.parse(checkInString, formatter);
+            LocalDate checkOutDate = LocalDate.parse(checkOutString, formatter);
+
+            nights = (int) ChronoUnit.DAYS.between(checkInDate, checkOutDate);
+            roomDetails = RoomService.getRoomDetailsById(Integer.parseInt(roomId));
+
+            double numberOfAdults = Double.parseDouble(fld_number_of_adult.getText());
+            double numberOfChildren = Double.parseDouble(fld_number_of_child.getText());
+
+            double totalPrice = ReservationService.calculatePrice(roomDetails, numberOfAdults, numberOfChildren, nights);
+            fld_total_price.setText(String.format("%.2f", totalPrice));
+        }catch (NumberFormatException e){
+            fld_total_price.setText("");
+        }
     }
 
 }
